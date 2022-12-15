505646c8e9d11f60ff04a8fd906d89bb4537f2c5
Tim Ridgely <tim@opendining.net>
2022-12-15 15:33:05 -0500

    Add window handling functions

--- src/android/nl/xservices/plugins/Insomnia.java
+++ src/android/nl/xservices/plugins/Insomnia.java
@@ -12,6 +12,13 @@ public class Insomnia extends CordovaPlugin {
   private static final String ACTION_KEEP_AWAKE = "keepAwake";
   private static final String ACTION_ALLOW_SLEEP_AGAIN = "allowSleepAgain";
 
+  private static final String ACTION_BRING_TO_FRONT = "bringToFront";
+  private static final String ACTION_CLEAR_BRING_TO_FRONT = "clearBringToFront";
+
+  private static final String ACTION_ADD_WINDOW_FLAGS = "addWindowFlags";
+  private static final String ACTION_CLEAR_WINDOW_FLAGS = "clearWindowFlags";
+  private static final String ACTION_GET_WINDOW_FLAGS = "getWindowFlags";
+
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     try {
@@ -34,6 +41,63 @@ public class Insomnia extends CordovaPlugin {
               }
             });
         return true;
+
+		} else if (ACTION_BRING_TO_FRONT.equals(action)) {
+			cordova.getActivity().runOnUiThread(
+				new Runnable() {
+				  public void run() {
+					cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+						WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+						WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+						WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+					callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+				  }
+				});
+			return true;
+
+		} else if (ACTION_CLEAR_BRING_TO_FRONT.equals(action)) {
+			cordova.getActivity().runOnUiThread(
+				new Runnable() {
+				  public void run() {
+					cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+												WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+												WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+												WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+					callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+				  }
+				});
+			return true;
+
+		} else if (ACTION_ADD_WINDOW_FLAGS.equals(action)) {
+			cordova.getActivity().runOnUiThread(
+				new Runnable() {
+				  public void run() {
+					cordova.getActivity().getWindow().addFlags(args.getInt(0));
+					callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+				  }
+				});
+			return true;
+
+		} else if (ACTION_CLEAR_WINDOW_FLAGS.equals(action)) {
+			cordova.getActivity().runOnUiThread(
+				new Runnable() {
+				  public void run() {
+					cordova.getActivity().getWindow().clearFlags(args.getInt(0));
+					callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+				  }
+				});
+			return true;
+
+		} else if (ACTION_GET_WINDOW_FLAGS.equals(action)) {
+			cordova.getActivity().runOnUiThread(
+				new Runnable() {
+				  public void run() {
+					int flags = cordova.getActivity().getWindow().getAttributes().flags;
+					callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, flags));
+				  }
+				});
+			return true;
 
       } else {
         callbackContext.error("insomnia." + action + " is not a supported function. Did you mean '" + ACTION_KEEP_AWAKE + "'?");
--- www/Insomnia.js
+++ www/Insomnia.js
@@ -9,6 +9,26 @@ Insomnia.prototype.allowSleepAgain = function (successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
 };
 
+Insomnia.prototype.bringToFront = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Insomnia", "bringToFront", []);
+};
+
+Insomnia.prototype.clearBringToFront = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Insomnia", "clearBringToFront", []);
+};
+
+Insomnia.prototype.addWindowFlags = function (flags, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Insomnia", "addWindowFlags", [flags]);
+};
+
+Insomnia.prototype.clearWindowFlags = function (flags, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Insomnia", "clearWindowFlags", [flags]);
+};
+
+Insomnia.prototype.getWindowFlags = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Insomnia", "getWindowFlags", []);
+};
+
 Insomnia.install = function () {
   if (!window.plugins) {
     window.plugins = {};
